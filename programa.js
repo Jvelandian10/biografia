729075511e5484cd3db7f0ba84b6fa60ceaf3c7d
Jvelandian10 <juliana.velandian@gmail.com>
2023-03-29 07:10:30 -0500

    cambio1

---
+++ programa.js
@@ -0,0 +1,16 @@
+
+// Comentarios de una linea
+
+/*
+Comentarios de varias lineas
+Documentar el código
+*/
+
+//No es necesario declarar una variable antes de utilizar
+
+//Variables de ambito de función
+var_a;
+//Variables de ámbito de bloque
+let b;
+
+//Operaciones: aritmeticas, comparacion, logica binaria (AND, OR)
